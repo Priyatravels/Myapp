9cbdf2471c26080d9402135d786f74c0f88fbda5
Shanmukha Priya TATUKURI <[priya]>
2023-03-14 16:10:41 +0000

    Login Page Added

---
+++ app/src/main/java/com/example/w9585275/myapp/LogIn.java
@@ -0,0 +1,14 @@
+package com.example.w9585275.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
